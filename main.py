600d00565e2dff0a1460d17aba87cce498291aa2
usercraf <ciber_pantera@icloud.com>
2025-07-09 13:40:32 +0300

    new

--- main.py
+++ main.py
@@ -1,61 +1,52 @@
 import asyncio
-import random
+
 from aiogram.filters.command import Command
 
 from aiogram import Bot, Dispatcher, types
 from aiogram.exceptions import TelegramNetworkError
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
-from key_file import TOKEN
+from key_file import TOKEN, buttons_students, button_admin
 from log_file import logger
 from db_file import cur, base
+from student_file import students_router
+from admin_file import admin_router
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
-def button_admin():
-    return {
-        'add_student':'🧑‍🎓 Додати студента',
-        'del_student': '❌ Видалити студента',
-        'add_stars': '🌟 Додати зірочки',
-        'add_home_work': '📚 Додати домашнє завдання',
-    }
-
-def buttons_students():
-    return {
-        'home_work_students':'📚 Домашні завдання',
-        'chek_start':'🌟 Перевірка зірочок',
-    }
-
-def generate_unique_code():
-    while True:
-        code = "{:06d}".format(random.randint(100000, 999999))
-        exists = cur.execute("SELECT 1 FROM students WHERE code = ?", (code,)).fetchone()
-        if not exists:
-            return int(code)
 
 @dp.message(Command("start"))
 async def start_bot(message: types.Message):
-    role = cur.execute("""SELECT role FROM students WHERE tg_id=?""", (message.from_user.id,)).fetchone()
+    data = cur.execute("""SELECT role, verification FROM students WHERE tg_id=?""", (message.from_user.id,)).fetchone()
     builder = InlineKeyboardBuilder()
-    if role[0] == 'ADMIN':
+
+    if data is None:
+        await message.answer('Вас не знайдено в базі даних зверніться до адміністратора.')
+        builder.add(types.InlineKeyboardButton(text='⬇️ Тут', callback_data='verification'))
+        await message.answer('Верифікуйся', reply_markup=builder.as_markup())
+
+    role, verification = data
+    if role == 'ADMIN':
         for key, value in button_admin().items():
             builder.add(types.InlineKeyboardButton(text=value, callback_data=key))
         builder.adjust(1)
         await message.answer(f'Вітаю адміністраторе {message.from_user.first_name}, що будемо робити?',
                              reply_markup=builder.as_markup())
-    elif role[0] == 'student':
+    elif role == 'student' and verification == 1:
         for key, value in buttons_students().items():
             builder.add(types.InlineKeyboardButton(text=value, callback_data=key))
         builder.adjust(1)
         await message.answer(f'Вітаю тебе {message.from_user.first_name}, що будемо робити?',
                              reply_markup=builder.as_markup())
     else:
-        builder.add(types.InlineKeyboardButton(text='Тут', callback_data='verification'))
+        builder.add(types.InlineKeyboardButton(text='⬇️ Тут', callback_data='verification'))
         await message.answer('Верифікуйся', reply_markup=builder.as_markup())
 
 
 async def main():
+    dp.include_router(students_router)
+    dp.include_router(admin_router)
     tryings = 0
     while True:
         try:
