fca689b070b6281952d0cf30a010632e09e86349
usercraf <ciber_pantera@icloud.com>
2025-07-10 15:59:53 +0300

    add exept

--- student_file.py
+++ student_file.py
@@ -1,11 +1,12 @@
 from aiogram import types, F, Router
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
+from aiogram.types import InlineKeyboardMarkup
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 from db_file import cur, base
 
 from log_file import logger
-from key_file import buttons_students
+from key_file import buttons_students, get_home_builder
 
 
 students_router = Router()
@@ -24,14 +25,12 @@ async def verification_student(callback: types.CallbackQuery, state: FSMContext)
 @students_router.message(Verification.verification)
 async def verification_code(message: types.Message, state: FSMContext):
     code = message.text.strip()
-    builder = InlineKeyboardBuilder()
-    builder.add(types.InlineKeyboardButton(text="🏠 На головну", callback_data="Home"))
     try:
         secret_code = cur.execute("""SELECT 1 FROM students WHERE secret_key=?""", (code,)).fetchone()
         if secret_code is None:
             logger.warning(
                 f"Користувач {message.from_user.first_name} (ID: {message.from_user.id}) ввів непривильний пароль до верифікації.")
-            await message.answer('❌ Ви ввели певірний код. Спробуйте ще раз')
+            await message.answer('❌ Ви ввели невірний код. Спробуйте ще раз')
             await state.set_state(Verification.verification)
         else:
             logger.info(
@@ -40,15 +39,16 @@ async def verification_code(message: types.Message, state: FSMContext):
             cur.execute("""UPDATE students SET verification=?, tg_id=? WHERE secret_key=?""", (1, int(message.from_user.id), code))
             base.commit()
             await state.clear()
-            await message.answer('🥳 Вітаю верифікація пройдена')
+            await message.answer('🥳 Вітаю верифікація пройдена.Натисніть /start.')
+
     except Exception as e:
         logger.error(f"[DB] Помилка під час SELECT: {e} функція - verification_code")
         await message.answer("⚠️ Помилка бази даних. "
-                             "Спробуйте пізніше або зверніться до адміністратора.", reply_markup=builder.as_markup())
+                             "Спробуйте пізніше або зверніться до адміністратора.", reply_markup=get_home_builder().as_markup())
         await state.clear()
 
 
-@students_router.callback_query(F.data == 'chek_start')
+@students_router.callback_query(F.data == 'chek_stars')
 async def chek_my_stars(callback: types.CallbackQuery):
     builder = InlineKeyboardBuilder()
     for key, value in buttons_students().items():
@@ -56,10 +56,53 @@ async def chek_my_stars(callback: types.CallbackQuery):
     builder.adjust(1)
     try:
         data_stars = cur.execute("""SELECT points FROM students WHERE tg_id=?""", (callback.from_user.id,)).fetchone()
-        await callback.message.edit_text(f'{callback.from_user.first_name} на Вашому рахунку {data_stars[0]} 🌟.',
+        await callback.message.answer(f'{callback.from_user.first_name} на Вашому рахунку {data_stars[0]} 🌟.',
                                       reply_markup=builder.as_markup())
     except Exception as e:
         logger.error(f'Помилка {e} під час перевірки наявності зірок у користувача {callback.from_user.id}')
         await callback.message.edit_text('❌ Сталася помилка, зверніться до адміністратора.',
                                          reply_markup=builder.as_markup())
 
+
+
+
+def get_paginated_keyboard(students: list, page: int, per_page: int = 5) -> InlineKeyboardMarkup:
+    builder = InlineKeyboardBuilder()
+
+    start = page * per_page
+    end = start + per_page
+    page_students = students[start:end]
+
+    for id_hw in page_students:
+        button_text = f"Домашнє завдання № {id_hw}"
+        builder.add(types.InlineKeyboardButton(text=button_text, callback_data=f"selecthw_{id_hw}"))
+
+    navigation = []
+    if page > 0:
+        navigation.append(types.InlineKeyboardButton(text="⬅️", callback_data=f"page_{page - 1}"))
+    if end < len(students):
+        navigation.append(types.InlineKeyboardButton(text="➡️", callback_data=f"page_{page + 1}"))
+
+    if navigation:
+        builder.adjust(1)
+        builder.row(*navigation)
+
+    return builder.as_markup()
+
+
+@students_router.callback_query(F.data == "home_work_students")
+async def show_hw_students(callback: types.CallbackQuery):
+    hw_data = cur.execute("SELECT id FROM hw_table").fetchall()
+    hw_list = [item[0] for item in hw_data]
+    keyboard = get_paginated_keyboard(hw_list, page=0)
+    await callback.message.answer("Перелік домашнього завдання:", reply_markup=keyboard)
+
+
+@students_router.callback_query(F.data.startswith("page_"))
+async def paginate_students(callback: types.CallbackQuery):
+    page = int(callback.data.split("_")[1])
+    hw_data = cur.execute("SELECT id FROM hw_table").fetchall()
+    hw_list = [item[0] for item in hw_data]
+    keyboard = get_paginated_keyboard(hw_list, page)
+    await callback.message.edit_reply_markup(reply_markup=keyboard)
+    await callback.answer()
