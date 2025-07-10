a45d00f824e9257a5e34a3670a2d77b791ab7b9c
usercraf <ciber_pantera@icloud.com>
2025-07-10 16:00:17 +0300

    add exept

--- admin_file.py
+++ admin_file.py
@@ -7,6 +7,7 @@ from db_file import cur, base
 import random
 
 from log_file import logger
+from key_file import get_home_builder
 
 admin_router = Router()
 
@@ -18,6 +19,9 @@ class AddStars(StatesGroup):
     get_stars = State()
     record_to_db = State()
 
+class DellStudent(StatesGroup):
+    del_student = State()
+
 def generate_unique_code():
     while True:
         code = "{:06d}".format(random.randint(100000, 999999))
@@ -28,7 +32,7 @@ def generate_unique_code():
 
 @admin_router.callback_query(F.data == 'add_student')
 async def get_name(callback: types.CallbackQuery, state: FSMContext):
-    await callback.message.answer('⬇️ Введіть імʼя студента.')
+    await callback.message.answer('⬇️ Введіть імʼя студента.', reply_markup=get_home_builder().as_markup())
     await state.set_state(AddStudent.name)
 
 @admin_router.message(AddStudent.name)
@@ -39,13 +43,14 @@ async def record_student(message: types.Message, state: FSMContext):
         cur.execute("""INSERT INTO students (full_name, secret_key) VALUES (?,?)""", (name_student, secret_key))
         base.commit()
         logger.info(f'Студента {name_student} успішно додано до бази даних.')
-        await message.answer(f'✅ Студента {name_student} внесено до бази даних.\n{secret_key} його ключ верифікації.')
+        await message.answer(f'✅ Студента {name_student} внесено до бази даних.\n{secret_key} ось ключ верифікації.',
+                             reply_markup=get_home_builder().as_markup())
         await state.clear()
 
     except Exception as e:
         logger.error(f'Виникла помилка при додаванні студента в базу даних {e}')
-        await message.answer('❌ Виникли проблеми з базою даних. Дивіться логи.')
-
+        await message.answer('❌ Виникли проблеми з базою даних. Дивіться Log.',
+                             reply_markup=get_home_builder().as_markup())
 
 @admin_router.callback_query(F.data == 'add_stars')
 async def all_students(callback: types.CallbackQuery, state: FSMContext):
@@ -59,13 +64,14 @@ async def all_students(callback: types.CallbackQuery, state: FSMContext):
         await state.set_state(AddStars.one_student)
     except Exception as e:
         logger.error(f'Сталась помилка {e} під час читання бази даних у функції all_students')
-        await callback.message.answer(text='❌ Сталась помилка зверніться до адміністратора.')
+        await callback.message.answer(text='❌ Сталась помилка зверніться до адміністратора.',
+                                      reply_markup=get_home_builder().as_markup())
         await state.clear()
 
 @admin_router.callback_query(AddStars.one_student, F.data.startswith('student_'))
 async def get_stars(callback: types.CallbackQuery, state: FSMContext):
     await state.update_data(tg_id=callback.data.split("_")[1])
-    await callback.message.answer('⬇️ Введіть кількість зірок яку маємо додати.')
+    await callback.message.answer('⬇️ Введіть кількість зірок яку маємо додати.', reply_markup=get_home_builder().as_markup())
     await state.set_state(AddStars.get_stars)
 
 @admin_router.message(AddStars.get_stars)
@@ -74,23 +80,52 @@ async def record_to_table(message: types.Message, state: FSMContext):
     data_fsm = await state.get_data()
     tg_id = data_fsm.get('tg_id')
     try:
-        get_stars = cur.execute("""SELECT points FROM students WHERE tg_id = ?""", (tg_id,)).fetchone()
+        get_star = cur.execute("""SELECT points FROM students WHERE tg_id = ?""", (tg_id,)).fetchone()
         logger.info(f'Вибір кількості зірок у користувача {tg_id}')
-        result_stars = int(get_stars[0]) + stars
+        result_stars = int(get_star[0]) + stars
         cur.execute("""UPDATE students SET points = ? WHERE tg_id = ?""", (result_stars, tg_id))
         base.commit()
-        await message.answer('✅ Ви успішно додали зірочки до користувача. Користувачу буде надіслано повідомлення про оновлення.')
+        await message.answer('✅ Ви успішно додали зірочки до користувача. '
+                             'Користувачу буде надіслано повідомлення про оновлення.', reply_markup=get_home_builder().as_markup())
         try:
             await message.bot.send_message(chat_id=tg_id, text=f'Вам нараховано {stars} зірочок. Так тримати!')
         except TelegramBadRequest as e:
-            logger.error(f'Користувач {tg_id} видалив чат або бот не має дозволу писати')
-            await message.answer('❌ Сталась помилка зверніться до адміністратора.')
+            logger.error(f'Користувач {tg_id} видалив чат або бот не має дозволу писати {e}.')
+            await message.answer(f'❌ Сталась помилка зверніться до адміністратора {e}.')
         except TelegramForbiddenError as e:
-            logger.error(f'Користувач {tg_id} заблокував бота')
-            await message.answer('❌ Сталась помилка зверніться до адміністратора.')
+            logger.error(f'Користувач {tg_id} заблокував бота {e}')
+            await message.answer(f'❌ Сталась помилка зверніться до адміністратора {e}.')
 
     except Exception as e:
         logger.error(f'Помилка {e} при роботи з БД функція record_to_table')
         await message.answer('❌ Сталася помилка, зверніться до адміністратора.')
         await state.clear()
 
+@admin_router.callback_query(F.data == 'del_student')
+async def chose_dell_student(callback: types.CallbackQuery, state: FSMContext):
+    try:
+        data_students = cur.execute("""SELECT full_name, tg_id FROM students WHERE role=?""", ('student',)).fetchall()
+        builder = InlineKeyboardBuilder()
+        for name, tg_id in data_students:
+            builder.add(types.InlineKeyboardButton(text=name, callback_data=f'dell_{tg_id}'))
+        builder.adjust(1)
+        await callback.message.answer('⬇️ Виберіть студента.', reply_markup=builder.as_markup())
+        await state.set_state(DellStudent.del_student)
+    except Exception as e:
+        logger.error(f'Сталась помилка {e} під час читання бази даних у функції all_students')
+        await callback.message.answer(text='❌ Сталась помилка зверніться до адміністратора.')
+        await state.clear()
+
+@admin_router.callback_query(DellStudent.del_student, F.data.startswith('dell_'))
+async def del_student(callback: types.CallbackQuery, state: FSMContext):
+    tg_id_user = callback.data.split('_')[1]
+    try:
+        cur.execute("""DELETE FROM students WHERE tg_id = ?""", (tg_id_user,))
+        logger.info(f'Відбулось видалення користувача {tg_id_user} з бази даних')
+        base.commit()
+        await callback.message.answer('✅ Успішно видалили студента.', reply_markup=get_home_builder().as_markup())
+        await state.clear()
+    except Exception as e:
+        logger.error(f'Відбулась помилка {e} при видаленні студента.')
+        await callback.message.answer(f'❌ Відбулась помилка.', reply_markup=get_home_builder().as_markup())
+        await state.clear()
